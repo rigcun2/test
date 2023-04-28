42e50bf59303f8b89523bbdd6e7d24b294931735
rigcun2 <132059122+rigcun2@users.noreply.github.com>
2023-04-28 23:32:45 +0300

    Update check.py

--- test/check.py
+++ test/check.py
@@ -3,7 +3,7 @@ import requests,fake_useragent,time,os,threading
 from threading import Thread
 from rich.console import Console
 from rich.progress import *
-payload = {"JoinFormTicket[gamecode]": "9627399"}
+payload = {"JoinForm[gamecode]": "9627399", "JoinForm[name]": "tester"}
 
 console = Console()
 
@@ -12,4 +12,4 @@ os.system('cls' if os.name == 'nt' else 'clear')
 console.print('''взломщик тестов
 ''')
 with requests.session() as s:
-    p=s.post('https://naurok.ua/student/tests', data=payload)
+    p=s.post('https://naurok.ua/test/join', data=payload)
