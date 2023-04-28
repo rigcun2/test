b6dcac0d93c7d7fb522064bd2c71db5ac7a76da3
rigcun2 <132059122+rigcun2@users.noreply.github.com>
2023-04-28 13:48:16 +0300

    Update Spavveri.py

--- test/Spavveri.py
+++ test/Spavveri.py
@@ -16,8 +16,8 @@ for i in range(999):
     for i in range(7):
         kod = str(random.choice('1234567890') + kod)
     headers = {"UserAgent": fake_useragent.UserAgent().random}
-         try:
-            requests.post("https://naurok.ua/student/tests", data={"phone": kod}, headers=headers, proxies=proxies)
+    try:
+        requests.post("https://naurok.ua/student/tests", data={"phone": kod}, headers=headers, proxies=proxies)
             print('мае')
-         except:
-            print('немае')
+    except:
+        print('немае')
