5fbef80c84b110353eadbfbf267f3740e14a04e1
rigcun2 <132059122+rigcun2@users.noreply.github.com>
2023-04-28 15:09:24 +0300

    Update check.py

--- test/check.py
+++ test/check.py
@@ -1,7 +1,18 @@
+import random
+import requests,fake_useragent,time,os,threading
+from threading import Thread
+from rich.console import Console
+from rich.progress import *
+
+console = Console()
+
+os.system('cls' if os.name == 'nt' else 'clear')
+
+console.print('''взломщик тестов
+''')
+
 for i in range(999):
-    kod = ''
-    for i in range(7):
-        kod = str(random.choice('1234567890') + kod)
+    kod = '9627399'
     headers = {"UserAgent": fake_useragent.UserAgent().random}
     try:
         requests.post("https://naurok.ua/test/join", data={"phone": kod}, headers=headers, proxies=proxies)
